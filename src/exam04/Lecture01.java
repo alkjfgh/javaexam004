51a4a08739d1c98174efa6ef402a7c4896b484c8
alkjfgh <alkjfgh@naver.com>
2020-08-31 10:39:18 +0900

    20200831

---
+++ src/exam04/Lecture01.java
@@ -0,0 +1,14 @@
+package exam04;
+
+public class Lecture01 {
+
+	public static void main(String[] args) {
+		Object objI = 120;
+		Object objJ = 147258L;
+		Object objK = "yangdoll";
+		int i = 120; long j =147258L; String k = "yangdoll";
+		System.out.println(i+"	"+j+"	"+k);
+		System.out.println(objI+"	"+objJ+"	"+objK);
+	}
+
+}
